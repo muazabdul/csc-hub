d338df44b2bfb27574bb50c9012bde0a2093ae58
Muazabdul <itsmyofcl@gmail.com>
2025-04-02 15:50:14 +0530

    Setup basic react structure

---
+++ csc-connect-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CSC Connect</h1>
+      {/* We will add content here */}
+    </div>
+  );
+}
+
+export default App;
